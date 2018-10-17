025260775490fc560e800b27a745372733861b98
Aian Shay <aianshay@gmail.com>
2018-10-17 09:00:30 -0300

    Update src/br/com/library/operacoes/Teste.java
    de novo

--- src/br/com/library/operacoes/Teste.java
+++ src/br/com/library/operacoes/Teste.java
@@ -4,6 +4,6 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("teste do git desgraça.");
 		
-		//testeando novamente kkkk
+		//testeando novamente kkkk usadhuad
 	}
 }
